5e522eeb54601db8d09f6b40181e2170f0758345
negineri <negineri@sfc.wide.ad.jp>
2025-04-07 22:19:14 +0900

    Update 50-install-mise.sh

--- run_onchange_50-install-mise.sh
+++ run_onchange_50-install-mise.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# 4月開始 2ヶ月に更新 12月と1月は同じ
+# {{ mod ( div ( now | date "01" | int64 | add 4 ) 2 ) 6 }}
+
 function install_mise() {
     echo "Installing Mise..."
     curl https://mise.run | sh
