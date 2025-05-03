daf8f2af95b82e7be68270b1ef6477cdefeb67e3
negineri <negineri@sfc.wide.ad.jp>
2025-05-04 01:03:16 +0900

    Update scripts/install-eza.sh

--- scripts/install-eza.sh
+++ scripts/install-eza.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-wget -c https://github.com/eza-community/eza/releases/latest/download/eza_x86_64-unknown-linux-gnu.tar.gz -O - | tar xz
+# システムアーキテクチャを検出
+ARCH=$(uname -m)
+
+if [ "$ARCH" = "aarch64" ] || [ "$ARCH" = "arm64" ]; then
+    # Raspberry Pi (aarch64/arm64)向けのバイナリをダウンロード
+    wget -c https://github.com/eza-community/eza/releases/latest/download/eza_aarch64-unknown-linux-gnu.tar.gz -O - | tar xz
+else
+    # デフォルト：x86_64向けのバイナリをダウンロード
+    wget -c https://github.com/eza-community/eza/releases/latest/download/eza_x86_64-unknown-linux-gnu.tar.gz -O - | tar xz
+fi
+
 chmod +x eza
 mv eza $HOME/.local/bin/eza
 curl -OL https://raw.githubusercontent.com/eza-community/eza/main/completions/zsh/_eza
