e15ec9c34516037fa5027f9a1e2139b88fc9a96e
negineri <negineri@sfc.wide.ad.jp>
2026-07-26 00:24:21 +0900

    Update .chezmoidata/packages.yaml Update .chezmoiexternal.toml Remove .amethyst.yml Remove .config/timewarrior/timewarrior.cfg Remove .emacs.d/private/org-roam/README.org Remove .emacs.d/private/org-roam/packages.el Remove .gemini/commands/semantic-commit.toml Remove .gemini/settings.json Update .p10k.zsh Remove .qwen/commands/semantic-commit.toml Remove .qwen/settings.json Remove .spacemacs Remove .task/hooks/on-modify.timewarrior Remove .taskopenrc Remove .taskrc Update .zshrc Update 50-install-mise.sh Remove scripts/install-emacs-plus.sh Remove scripts/install-org-roam.sh Remove scripts/install-spacemacs.sh Remove scripts/install-taskopen.sh Remove scripts/install-taskwarrior-tui.sh Remove scripts/install-timewarrior-hook.sh

--- scripts/install-org-roam.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-if [ ! -d $HOME/org-roam ]; then
-    echo "clone org-roam"
-    git clone git@github.com:negineri/roam.git $HOME/org-roam
-fi
--- scripts/install-taskwarrior-tui.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /tmp
-curl https://github.com/kdheepak/taskwarrior-tui/releases/latest/download/taskwarrior-tui-x86_64-unknown-linux-musl.tar.gz -Lo - | tar xz
-mkdir -p $HOME/.local/bin
-mv taskwarrior-tui $HOME/.local/bin/
